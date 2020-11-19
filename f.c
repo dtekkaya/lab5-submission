43f29becaedb798e2fea528fbcda036c2cbc1270
dtekkaya <72623647+dtekkaya@users.noreply.github.com>
2020-11-19 22:55:07 +0300

    Update f.c

--- f.c
+++ f.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+
+
 int ask number(){
   int number=0;
   printf("Enter a number between 0 and 10");
@@ -5,8 +8,16 @@ int ask number(){
   if(number<10 && number>0){
     return (number)
   }else{
-    printf("Please a different number");
+    printf("Please enter a different number that is in range");
     scanf("d",&number);
   }
 }
 
+void print result(int result){
+  printf("The result is %d", &result);
+}
+  
+  
+  
+  
+  
