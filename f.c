dc61fcf1425921043b5d3fa225adb11aadee8873
dtekkaya <72623647+dtekkaya@users.noreply.github.com>
2020-11-19 22:52:43 +0300

    Update f.c

--- f.c
+++ f.c
@@ -1,1 +1,12 @@
+int ask number(){
+  int number=0;
+  printf("Enter a number between 0 and 10");
+  scanf("%d",&number);
+  if(number<10 && number>0){
+    return (number)
+  }else{
+    printf("Please a different number");
+    scanf("d",&number);
+  }
+}
 
